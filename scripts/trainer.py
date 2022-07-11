ce21c84bb59660982b3fe216cad8249d9d62c289
mibbim <michele.berti.g@gmail.com>
2022-07-11 17:45:10 +0200

    added model to trainer initialisation and removed from train method.

--- scripts/trainer.py
+++ scripts/trainer.py
@@ -12,28 +12,28 @@ from torch.utils.data import DataLoader
 
 from tqdm.auto import trange
 
-from DiffusionModel import DiffusionModel
-from DiffusionModel import Loss, Device
-from DiffusionModel import default_device
+from .DiffusionModel import DiffusionModel, default_model
+from .DiffusionModel import Loss
+from .utils import default_device, Device
 
-from performance_meter import AverageMeter
+from .performance_meter import AverageMeter
 
 optimizers_dict = {"Adam": torch.optim.Adam}
 
 
 class Trainer:
     def __init__(self,
+                 model: DiffusionModel = default_model(),
                  optimizer: Literal["Adam"] | None = "Adam",
                  learning_rate: float | None = 1e-3,
 
                  ) -> None:
-
-        self.opt = optimizers_dict[optimizer](learning_rate)
+        self.model = model
+        self.opt = optimizers_dict[optimizer](params=model.parameters(), lr=learning_rate)
         self.history = {"loss": []}
         # self.lr = learning_rate
 
     def train(self,
-              diff_model: DiffusionModel,
               n_epochs: int,
               train_dataloader: DataLoader,
               device: Device = default_device,
@@ -41,7 +41,7 @@ class Trainer:
               ) -> None:
 
         # Set the module in training mode
-        diff_model.train()
+        self.model.train()
 
         epochs = trange(n_epochs, desc="Training epoch")
 
@@ -49,7 +49,7 @@ class Trainer:
         for epoch in epochs:
             for x, y in train_dataloader:
                 x = x.to(device)  # Batch of Images
-                loss = diff_model.train_step(x, self.opt, loss_function)
+                loss = self.model.train_step(x, self.opt, loss_function)
                 average_meter.update({"train_mse": loss.item()}, n=x.shape[0])
 
             self.history["loss"].append(average_meter.metrics["train_mse"]["avg"])
