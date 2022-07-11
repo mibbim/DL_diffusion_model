e9323d719947f9a50f20ed2668e610888b576b39
mibbim <michele.berti.g@gmail.com>
2022-07-11 13:22:15 +0200

    Changed dict structure in AverageMeter

--- scripts/performance_meter.py
+++ scripts/performance_meter.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from abc import ABC
-from typing import Dict
+from typing import Dict, Iterable
 
 import torch
 
@@ -24,32 +24,39 @@ class AverageMeter(PerformanceMeter):
     https://github.com/ansuini/DSSC_DL_2022/blob/main/labs/03-sgd-training.ipynb
     """
 
-    def __init__(self, metrics):
-        self.metrics = metrics
-        self.val = {}
-        self.count = {}
-        self.sum = {}
-        self.avg = {}
-        self.reset()
+    def __init__(self, metrics: Iterable[str]):
+        self._reset_metric_dict = {
+            "val": 0,
+            "count": 0,
+            "sum": 0,
+            "avg": 0,
+        }
+        self.metrics = {}
+        self.reset(metrics)
 
     def _reset_single_metric(self, metric: str) -> None:
-        self.val[metric] = 0  # val holds the current stat
-        self.avg[metric] = 0  # avg holds the cumulative average
-        self.sum[metric] = 0  # sum holds the cumulative value
-        self.count[metric] = 0  # count holds the number of instances seen
+        self.metrics[metric] = self._reset_metric_dict
 
-    def _update_single_metric(self, val: float,
+    def _update_single_metric(self,
                               metric: str,
+                              val: float,
                               n: int | None = 1) -> None:
-        self.val[metric] = val
-        self.count[metric] += n
-        self.sum[metric] += val * n
-        self.avg[metric] = self.sum[metric] / self.count[metric]
+
+        self.metrics[metric]["val"] = val
+        self.metrics[metric]["count"] += n
+        self.metrics[metric]["sum"] += val * n
+        self.metrics[metric]["avg"] = self.metrics[metric]["sum"] / self.metrics[metric]["count"]
 
     def update(self, values_dict: Dict[str, torch.Tensor], n: int | None = 1) -> None:
         for metric, val in values_dict:
-            self._update_single_metric(val, metric, n)
+            self._update_single_metric(metric, val, n)
 
-    def reset(self) -> None:
-        for metric, val in self.metrics:
+    def reset(self, metrics_list: Iterable[str] | None = None) -> None:
+        if metrics_list is None:
+            metrics_list = self.metrics.keys()
+        for metric in metrics_list:
             self._reset_single_metric(metric)
+
+
+if __name__ == "__main__":
+    pass
