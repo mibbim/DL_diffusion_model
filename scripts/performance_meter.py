dd31e283b4beead023c73d153be3d2b68c47a249
mibbim <michele.berti.g@gmail.com>
2022-07-12 14:26:35 +0200

    Movet performance meter test to test unit

--- scripts/performance_meter.py
+++ scripts/performance_meter.py
@@ -62,18 +62,3 @@ class AverageMeter(PerformanceMeter):
             metrics_list = self.metrics.keys()
         for metric in metrics_list:
             self._reset_single_metric(metric)
-
-
-def test_avg_meter():
-    am = AverageMeter(["mse", "vlb"])
-    print(f"Instatiated with mse and vlb: \n", am.metrics)
-    am.update({"mse": 10, "vlb": 3})
-    print(f"Updated with 10, and 3: \n", am.metrics)
-    am.update({"mse": 30, "vlb": 5})
-    print(f"Updated with 30 and 5: \n", am.metrics)
-    am.reset()
-    print(f"Reset: \n", am.metrics)
-
-
-if __name__ == "__main__":
-    test_avg_meter()
