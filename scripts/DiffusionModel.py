a0f3fc70aa7add582b87471e22351a511ab20f05
mibbim <michele.berti.g@gmail.com>
2022-07-13 12:55:58 +0200

    Created class beta

--- scripts/DiffusionModel.py
+++ scripts/DiffusionModel.py
@@ -15,6 +15,7 @@ from .utils import default_device, Device
 
 IDT = TypeVar("IDT")  # Input Data Type
 Loss = TypeVar("Loss")  # Loss function object
+BetaSchedule = Literal["linear"]  # TypeVar("BetaSchedule")
 
 
 def default_model():
@@ -26,13 +27,63 @@ def default_model():
     ).to(default_device)
 
 
+class LinearBeta:
+    def __init__(self,
+                 lower: float | None = 0.0001,
+                 upper: float | None = 0.04,
+                 steps: int = 100,
+                 device: Device | None = default_device):
+        self.min = lower
+        self.max = upper
+        self.steps = steps
+        self.betas = torch.linspace(self.min, self.max, self.steps).to(device)
+        self._alphas = 1. - self.betas
+        self.alpha_prod = torch.cumprod(self._alphas, dim=0)
+
+
+class NoiseGenerator:
+    def __init__(self,
+                 max_diff_steps: int | None = 100,
+                 beta: LinearBeta = LinearBeta(),
+                 # min_beta: float | None = 0.0001,
+                 # max_beta: float | None = 0.04,
+                 # beta_schedule: BetaSchedule | None = "linear",
+                 device: Device | None = default_device
+                 ):
+        self.device = device
+        self.beta = beta
+        self.max_diff_steps = self.beta.steps
+
+    def _sample_t(self, x) -> LongTensor:
+        """Samples the number of diffusion steps to apply to the batch x"""
+        return torch.randint(0, self.max_diff_steps, (x.shape[0],),
+                             dtype=torch.long).to(self.device)
+
+    @staticmethod
+    def _sample_noise(x: IDT) -> IDT:
+        """Samples the noise to add to the batch of image"""
+        return torch.randn_like(x).to(x.device)
+
+    def add_noise(self, x: IDT, t: LongTensor | None = None) -> Tuple[IDT, IDT, LongTensor]:
+        """Returns the noisy images, the noise, and the sampled times"""
+        if t is None:
+            t = self._sample_t(x)
+        noise = self._sample_noise(x)
+        alpha_prod_t = self.beta.alpha_prod.gather(-1, t).reshape(-1, 1, 1, 1)
+        mean = x * alpha_prod_t.sqrt()
+        std = (1 - alpha_prod_t).sqrt()
+        return mean + noise.mul(std), noise, t
+
+
 class DiffusionModel(nn.Module):  # Not sure should inherit
     def __init__(self,
                  noise_predictor: nn.Module,
+                 noise_generator: NoiseGenerator | None = NoiseGenerator(),
                  diffusion_steps_num: int | None = 100,
                  evaluation_device: Device | None = default_device,
                  ) -> None:
         super().__init__()  # Not sure should inherit
+        self._noise_generator = noise_generator
         self._noise_predictor = noise_predictor
         self.max_diff_steps = diffusion_steps_num
         self.device = evaluation_device
@@ -64,10 +115,7 @@ class DiffusionModel(nn.Module):  # Not sure should inherit
                    optimizer: Optimizer,
                    loss_fun: Loss,
                    ):
-        noise_generator = NoiseGenerator(self.max_diff_steps,
-                                         self.betas[0],
-                                         self.betas[-1])
-        noisy_x, noise, t = noise_generator.add_noise(x)
+        noisy_x, noise, t = self._noise_generator.add_noise(x)
         predicted_noise = self._noise_predictor(x, t)
         loss = loss_fun(noise, predicted_noise)
         optimizer.zero_grad()
@@ -76,50 +124,7 @@ class DiffusionModel(nn.Module):  # Not sure should inherit
         return loss
 
     def val_step(self, x, validation_metric):
-        noise_generator = NoiseGenerator(self.max_diff_steps,
-                                         self.betas[0],
-                                         self.betas[-1])
-        noisy_x, noise, t = noise_generator.add_noise(x)
+        noisy_x, noise, t = self._noise_generator.add_noise(x)
         predicted_noise = self._noise_predictor(x, t)
         loss = validation_metric(noise, predicted_noise)
         return loss
-
-
-BetaSchedule = Literal["linear"]  # TypeVar("BetaSchedule")
-
-
-class NoiseGenerator:
-    def __init__(self,
-                 max_diff_steps: int | None = 100,
-                 min_beta: float | None = 0.0001,
-                 max_beta: float | None = 0.04,
-                 beta_schedule: BetaSchedule | None = "linear",
-                 device: Device | None = default_device
-                 ):
-        self.device = device
-        self.max_diff_steps = max_diff_steps
-        if beta_schedule == "linear":
-            self.betas = torch.linspace(min_beta, max_beta, self.max_diff_steps).to(self.device)
-        self._alphas = 1. - self.betas
-        self._alpha_prod = torch.cumprod(self._alphas, dim=0)
-
-    def _sample_t(self, x) -> LongTensor:
-        """Samples the number of diffusion steps to apply to the batch x"""
-        return torch.randint(0, self.max_diff_steps, (x.shape[0],),
-                             dtype=torch.long).to(self.device)
-
-    @staticmethod
-    def _sample_noise(x: IDT) -> IDT:
-        """Samples the noise to add to the batch of image"""
-        return torch.randn_like(x).to(x.device)
-
-    def add_noise(self, x: IDT, t: LongTensor | None = None) -> Tuple[IDT, IDT, LongTensor]:
-        """Returns the noisy images, the noise, and the sampled times"""
-        if t is None:
-            t = self._sample_t(x)
-        noise = self._sample_noise(x)
-        alpha_prod_t = self._alpha_prod.gather(-1, t).reshape(-1, 1, 1, 1)
-        # self._alpha_prod.gather(-1, t).reshape(-1, 1, 1, 1)
-        mean = x * alpha_prod_t.sqrt()
-        std = (1 - alpha_prod_t).sqrt()
-        return mean + noise.mul(std), noise, t
