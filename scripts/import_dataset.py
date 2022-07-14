1746fda39f7fda821f879e99c8dcf4fe384c62cc
Thomas <thomas.verardo1@gmail.com>
2022-07-14 12:16:15 +0200

    Comment on main

--- scripts/import_dataset.py
+++ scripts/import_dataset.py
@@ -80,19 +80,19 @@ def load_data_CSSD(train_batch_size, test_batch_size, ratio_test=0.20, verbose=F
 # data_train, data_test = load_data(train_batch_size=32, test_batch_size=32, ratio_data=100)
 
 
-def test():
+# def test():
 
-    torch.manual_seed(8)
-    # train_loader, test_loader = load_MNIST(1, 1, 10000, verbose=True)  # [1, 1, 28, 28] 1 is the number of img in the batch, 1 is the number of channel, 28x28 pixels
-    train_loader, test_loader = load_data_CSSD(1, 1, 0.20, verbose=True) 
-    x = next(iter(train_loader))
-    print(f"x is a {type(x)} of shape {x.shape}") 
-    img = T.ToPILImage()(x[0])
-    img.show()
-    pass
+#     torch.manual_seed(8)
+#     # train_loader, test_loader = load_MNIST(1, 1, 10000, verbose=True)  # [1, 1, 28, 28] 1 is the number of img in the batch, 1 is the number of channel, 28x28 pixels
+#     train_loader, test_loader = load_data_CSSD(1, 1, 0.20, verbose=True) 
+#     x = next(iter(train_loader))
+#     print(f"x is a {type(x)} of shape {x.shape}") 
+#     img = T.ToPILImage()(x[0])
+#     img.show()
+#     pass
 
 
-if __name__ == "__main__":
-    test()
+# if __name__ == "__main__":
+#     test()
 
 # >>>>>>> new_data
