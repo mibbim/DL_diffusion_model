7560b693957bccd9e5b3b86f93a79aea92052800
Valeria Insogna <valinsogna@gmail.com>
2022-07-14 12:39:33 +0200

    test Unet_valeria

--- test/test_Unet_valeria.py
+++ test/test_Unet_valeria.py
@@ -1,15 +1,20 @@
 import torch
 
-from scripts.Unet import Generator
-from scripts.utils import default_device
+from scripts.Unet_valeria import ConvBlockDownsample, ConvBlockUpsample
+#from scripts.utils import default_device
 
 
 def test_random_image():
-    img_channels = 1
-    img_size = 28
-    x = torch.randn((2, img_channels, img_size, img_size))
-    gen = Generator(img_channels, img_channels)
-    print(gen(x, 1).shape)
+    # img_channels = 1
+    # img_size = 28
+    # x = torch.randn((2, img_channels, img_size, img_size))
+    # gen = Generator(img_channels, img_channels)
+    # print(gen(x, 1).shape)
+    block2 = ConvBlockUpsample(128, 64)
+    data_before_upsample = torch.rand((1, 128, 12, 12))
+    # we do the forward pass by output_for_upsample from before
+    output2 = block2(data_before_upsample, output_for_upsample)
+    print("Shape of output2: ", output2.shape) #([1, 64, 20, 20])
 
 
 # def test_mnist():
