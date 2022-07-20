a9e95806e87992cff1864a2cb39532c32835c10a
Valeria Insogna <valinsogna@gmail.com>
2022-07-20 11:31:56 +0200

    Test_rete1

--- test/test_trainer.py
+++ test/test_trainer.py
@@ -27,6 +27,21 @@ def test_train():
     # plt.legend()
     # plt.show()
 
+def test_train_Unet_valeria():
+    trainer = Trainer(out_path=script_dir / "out")
+    train_loader, test_loader = load_MNIST(train_batch_size=32,
+                                           test_batch_size=16,
+                                           ratio_data=1000)
+    trainer.train(n_epochs=10,
+                  train_dataloader=train_loader,
+                  val_dataloader=test_loader,
+                  valid_each=2,
+                  )
+
+    # for m in trainer.history.keys():
+    #     plot_single(trainer.history, m)
+    # plt.legend()
+    # plt.show()
 
 if __name__ == "__main__":
     test_train()
