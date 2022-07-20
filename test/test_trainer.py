4e0f837db0a2c87bb9e5673cdfebbec5ab7c9d2c
mibbim <michele.berti.g@gmail.com>
2022-07-20 12:41:31 +0200

    Added test with valeria's unet

--- test/test_trainer.py
+++ test/test_trainer.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
+from scripts.DiffusionModel import DiffusionModel
+from scripts.Unet_valeria import UNet
 from scripts.trainer import Trainer
-from scripts.import_dataset import load_MNIST
+from scripts.import_dataset import load_MNIST, load_data_CSSD
 
 script_dir = Path(__file__).resolve().parent
 
@@ -27,11 +29,16 @@ def test_train():
     # plt.legend()
     # plt.show()
 
+
 def test_train_Unet_valeria():
-    trainer = Trainer(out_path=script_dir / "out")
-    train_loader, test_loader = load_MNIST(train_batch_size=32,
-                                           test_batch_size=16,
-                                           ratio_data=1000)
+    trainer = Trainer(model=DiffusionModel(noise_predictor=UNet(3, 3)
+
+                                           ),
+                      out_path=script_dir / "out",
+                      )
+    train_loader, test_loader = load_data_CSSD(train_batch_size=32,
+                                               test_batch_size=16,
+                                               ratio_test=0.2)
     trainer.train(n_epochs=10,
                   train_dataloader=train_loader,
                   val_dataloader=test_loader,
@@ -43,5 +50,7 @@ def test_train_Unet_valeria():
     # plt.legend()
     # plt.show()
 
+
 if __name__ == "__main__":
-    test_train()
+    # test_train()
+    test_train_Unet_valeria()
