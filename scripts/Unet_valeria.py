61d90449458c092f729813916b5229f72437513d
Valeria Insogna <valinsogna@gmail.com>
2022-07-21 23:22:14 +0200

    Better UNet_valeria

--- scripts/Unet_valeria.py
+++ scripts/Unet_valeria.py
@@ -1,5 +1,5 @@
 """
-This file contains the implementation of Valeria's U-net module,
+This file contains our implementation of the U-net module,
 used in DiffusionModel to predict the noise
 """
 import torch.nn as nn
@@ -56,18 +56,12 @@ class ActivationFunc(nn.Module):
 
 # Time Embedding position encoder
 class SinusoidalPositionEmbeddings(nn.Module):
-    # \begin{align}
-    # PE^{(1)}_{t,i} &= sin\Bigg(\frac{t}{10000^{\frac{i}{d - 1}}}\Bigg) \\
-    # PE^{(2)}_{t,i} &= cos\Bigg(\frac{t}{10000^{\frac{i}{d - 1}}}\Bigg)
-    # \end{align}
-    #
-    # where $d$ is `half_dim`
     """
     ### Sinusoidal position embeddings to encode time-step t, inspired by the Transformer (Vaswani et al., 2017).
 
     It takes a tensor of shape (batch_size, 1) as input (i.e. the noise levels of several noisy images in a batch), 
     and turns this into a tensor of shape (batch_size, dim), with dim being the dimensionality of the position embeddings. 
-    This is then added to each residual block, as we will see further.
+    This is then added to each double convblock.
 
     ----------
     #### Parameters
@@ -101,7 +95,7 @@ class SinusoidalPositionEmbeddings(nn.Module):
 # Double convolutional layer for Unet
 class DoubleConvBlock(nn.Module):
     """
-    #### Double convolutional block has two convolution layers with batch normalization
+    #### Double convolutional block has two convolution layers with batch normalization and same padding
     Specify custom activation function to use, other than default ReLU
 
     ----------
@@ -128,7 +122,7 @@ class DoubleConvBlock(nn.Module):
         layers = nn.ModuleList([
             nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3,
                       bias=False, padding=(1, 1)), #SAME padding
-            # stride=1,padding=0,bias set false since usage of BatchNorm
+            # stride=1,padding=1,bias set false since usage of BatchNorm
             nn.BatchNorm2d(out_channels),  # dimensionality of the incoming data
             ActivationFunc(activation_type)])
         if dropout:
@@ -161,15 +155,15 @@ class DoubleConvBlock(nn.Module):
         if self.time_emb:
             t = self.time_emb(t)
             # Add time embeddings
-            # x += t[:, :, None, None]
             x = x + t[:, :, None, None]
 
         return self.conv_block2(x)
 
-# Ahh yes, magical attention...
+
+# Attention block
 class AttentionBlock(nn.Module):
     """
-    ### Attention block.
+    ### Attention block is Transformer architecture from Vaswani et al., 2017 (https://arxiv.org/pdf/1706.03762.pdf)
     """
 
     def __init__(self, n_channels: int, n_heads: int = 1, d_k: int = None, n_groups: int = 32):
@@ -177,7 +171,7 @@ class AttentionBlock(nn.Module):
         * `n_channels` is the number of channels in the input
         * `n_heads` is the number of heads in multi-head attention
         * `d_k` is the number of dimensions in each head
-        * `n_groups` is the number of groups for [group normalization](../../normalization/group_norm/index.html)
+        * `n_groups` is the number of groups for [group normalization]
         """
         super().__init__()
 
@@ -232,6 +226,7 @@ class AttentionBlock(nn.Module):
         #
         return res
 
+
 # Module for downsampling
 class ConvBlockDownsample(nn.Module):
     """
@@ -277,10 +272,10 @@ class ConvBlockDownsample(nn.Module):
         # Add to list
         upsample_list.append(out_for_upsample)
 
-        # Resize data to avoid loss in MaxPooling
+        # Resize data to avoid loss in MaxPooling when size of image is odd
         if out_for_upsample.size(dim=2) % 2 != 0:
-            out_for_upsample = nn.ReplicationPad2d((0, 1, 0, 1))(
-                out_for_upsample)  # padding_left,padding_right,padding_top,padding_bottom
+            out_for_upsample = nn.ReplicationPad2d((0, 1, 0, 1))( # padding_left,padding_right,padding_top,padding_bottom
+                out_for_upsample)  
 
         # MaxPooling with stride=2, kernel=2
         out = self.downsample(out_for_upsample)
@@ -329,14 +324,14 @@ class ConvBlockUpsample(nn.Module):
         # with H > h, W > w
         h, w = out.shape[2], out.shape[3]
 
+        # Resize data to match the added padding in Downsample when size of image is odd
         if h % 2 != 0:
-            # padding_left,padding_right,padding_top,padding_bottom
-            out = nn.ReplicationPad2d((0, 1, 0, 1))(out)
+            out = nn.ReplicationPad2d((0, 1, 0, 1))(out)# padding_left,padding_right,padding_top,padding_bottom
 
         data_previous = data_from_downsample.pop()
         H, W = data_previous.shape[2], data_previous.shape[3]
 
-        # Do a center crop of data_from_downsample 
+        # Center crop of data_from_downsample if in DoubleConvBlock SAME padding is not used 
         # (starting from H//2, W//2, the center pixel of the larger image)
         if data_previous.shape != out.shape:
             cropper = CenterCrop((out.shape[-2], out.shape[-1]))
@@ -364,9 +359,9 @@ class OutConv(nn.Module):
     
     """
 
-    def __init__(self, in_channels, out_channels: int):
+    def __init__(self, in_channels, n_classes: int):
         super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.conv = nn.Conv2d(in_channels, n_classes, kernel_size=1)
 
     def forward(self, x):
         return self.conv(x)
@@ -430,8 +425,6 @@ class UNet(nn.Module):
         assert (
                 n_unet_blocks >= 0 and n_unet_blocks % 2 == 1), "n_unet_blocks must be an odd number"
         super(UNet, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
 
         # # First block
         # model = [ConvBlockDownsample(n_channels, n_conv_filters, dropout=dropout)]
@@ -465,31 +458,17 @@ class UNet(nn.Module):
                                          time_channels=n_conv_filters * 4, has_attn=is_attn[1])
         self.down3 = ConvBlockDownsample(n_conv_filters * 2, n_conv_filters * 4, dropout=dropout,
                                          time_channels=n_conv_filters * 4, has_attn=is_attn[2])
-        # self.down4 = ConvBlockDownsample(n_conv_filters * 4, n_conv_filters * 8, dropout=dropout,
-        #                                  time_channels=n_conv_filters * 4)
 
         # Middle blocks
-        # self.ground = DoubleConvBlock(n_conv_filters * 8, n_conv_filters * 16, dropout=dropout,
-        #                               time_channels=n_conv_filters * 4)
-        # self.ground = DoubleConvBlock(n_conv_filters * 4, n_conv_filters * 8, dropout=dropout,
-        #                               time_channels=n_conv_filters * 4)
         self.ground = MiddleBlock(n_conv_filters * 4, dropout=dropout,
                                          time_channels=n_conv_filters * 4)
 
         # Upsample blocks
-        # self.up1 = ConvBlockUpsample(n_conv_filters * 16, n_conv_filters * 8, dropout=dropout,
-        #                              time_channels=n_conv_filters * 4)
-        # self.up2 = ConvBlockUpsample(n_conv_filters * 8, n_conv_filters * 4, dropout=dropout,
-        #                              time_channels=n_conv_filters * 4, has_attn=is_attn[2])
-        # self.up3 = ConvBlockUpsample(n_conv_filters * 4, n_conv_filters * 2, dropout=dropout,
-        #                              time_channels=n_conv_filters * 4, has_attn=is_attn[1])
-        # self.up4 = ConvBlockUpsample(n_conv_filters * 2, n_conv_filters, dropout=dropout,
-        #                              time_channels=n_conv_filters * 4, has_attn=is_attn[0])
-        self.up2 = ConvBlockUpsample(n_conv_filters * 8, n_conv_filters * 4, dropout=dropout,
+        self.up1 = ConvBlockUpsample(n_conv_filters * 8, n_conv_filters * 4, dropout=dropout,
                                      time_channels=n_conv_filters * 4, has_attn=is_attn[2])
-        self.up3 = ConvBlockUpsample(n_conv_filters * 4, n_conv_filters * 2, dropout=dropout,
+        self.up2 = ConvBlockUpsample(n_conv_filters * 4, n_conv_filters * 2, dropout=dropout,
                                      time_channels=n_conv_filters * 4, has_attn=is_attn[1])
-        self.up4 = ConvBlockUpsample(n_conv_filters * 2, n_conv_filters, dropout=dropout,
+        self.up3 = ConvBlockUpsample(n_conv_filters * 2, n_conv_filters, dropout=dropout,
                                      time_channels=n_conv_filters * 4, has_attn=is_attn[0])
         # Final block
         self.outc = OutConv(n_conv_filters, n_classes)
@@ -500,33 +479,28 @@ class UNet(nn.Module):
         * `t` has shape `[batch_size]`
         """
         img_size = (x.shape[-2], x.shape[-1])
+        
         # Get time-step embeddings
         t = self.time_emb(t)
         up = list()
-        x1, up = self.down1(x, up, t)
-        x2, up = self.down2(x1, up, t)
-        x, up = self.down3(x2, up, t)
-        # x4, up = self.down4(x3, up, t)
+
+        x, up = self.down1(x, up, t)
+        x, up = self.down2(x, up, t)
+        x, up = self.down3(x, up, t)
+
         x = self.ground(x, t)
-        # x = self.up1(x5, up, t)
+
+        x = self.up1(x, up, t)
         x = self.up2(x, up, t)
         x = self.up3(x, up, t)
-        x = self.up4(x, up, t)
         x = self.outc(x)
+
+        # Resize to the original size for odd cases
         x = CenterCrop(img_size)(x)
         return x
 
 
 if __name__ == "__main__":
-    # m = nn.MaxPool2d(kernel_size=2)
-    # input = torch.rand(1, 1, 3, 3)
-    # print(input)
-    # output = m(input)
-    # print(output.shape)
-    # print(output)
-    # SinusoidalPositionEmbeddings
-    # # n_classes is the number of probabilities you want to get per pixel
-
     # Let's see it in action on dummy data:
     # A dummy batch of 1 3-channel 572px images
     x = torch.randn(1, 3, 32, 32)
@@ -543,8 +517,4 @@ if __name__ == "__main__":
     # The output shape matches the input.
     print("Shape of output: ", model_output.shape)
 
-    # net = UNet(n_channels=3, n_classes=1, dropout=0.5)
-    # data = torch.rand(1, 3, 572, 572)
-    # output = net(data)
-    # print("Shape of output: ", output.shape)
-    # print("UNet number of param: ", sum(p.numel() for p in unet.parameters() if p.requires_grad)) #31876673 aka 31 milioni di trainable parameters
+    print("UNet number of param: ", sum(p.numel() for p in unet.parameters() if p.requires_grad)) #31876673 aka 31 milioni di trainable parameters
