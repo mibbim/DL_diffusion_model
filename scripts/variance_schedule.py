2bb9cc857927efb4e8f817c282b6f37cbd39254f
mibbim <michele.berti.g@gmail.com>
2022-07-22 10:42:33 +0200

    Added Cosine schedule + refactoring

--- scripts/variance_schedule.py
+++ scripts/variance_schedule.py
@@ -1,54 +1,18 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
+from abc import ABC
 from .utils import default_device, Device
 
 import torch
 from torch import Tensor
+from numpy import pi
 
 
 class VarianceSchedule(ABC):
-
-    @property
-    @abstractmethod
-    def betas(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    def get_betas_t(self, t: Tensor):
-        raise NotImplementedError
-
-    @property
-    @abstractmethod
-    def alphas(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    def get_alphas_t(self, t: Tensor):
-        raise NotImplementedError
-
-    @property
-    @abstractmethod
-    def alpha_prod(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    def get_alpha_prod_t(self, t: Tensor):
-        raise NotImplementedError
-
-
-class LinearVarianceSchedule(VarianceSchedule):
-    def __init__(self,
-                 lower: float | None = 0.0001,
-                 upper: float | None = 0.04,
-                 steps: int = 100,
-                 device: Device | None = default_device):
-        self.min = lower
-        self.max = upper
-        self.steps = steps
-        self._betas = torch.linspace(self.min, self.max, self.steps).to(device)
-        self._alphas = 1. - self.betas
-        self._alpha_prod = torch.cumprod(self._alphas, dim=0)
+    steps: int
+    _betas: torch.Tensor
+    _alphas: torch.Tensor
+    _alpha_prod: torch.Tensor
 
     @property
     def betas(self):
@@ -70,3 +34,37 @@ class LinearVarianceSchedule(VarianceSchedule):
 
     def get_alphas_t(self, t: Tensor):
         return self._alphas.gather(-1, t).reshape(-1, 1, 1, 1)
+
+
+class LinearVarianceSchedule(VarianceSchedule):
+    def __init__(self,
+                 lower: float | None = 0.0001,
+                 upper: float | None = 0.04,
+                 steps: int = 100,
+                 device: Device | None = default_device):
+        self.min = lower
+        self.max = upper
+        self.steps = steps
+        self._betas = torch.linspace(self.min, self.max, self.steps).to(device)
+        self._alphas = 1. - self.betas
+        self._alpha_prod = torch.cumprod(self._alphas, dim=0)
+
+
+class CosineVarianceSchedule(VarianceSchedule):
+    def __init__(self,
+                 lower: float | None = 0.0001,
+                 upper: float | None = 0.04,
+                 steps: int = 100,
+                 device: Device | None = default_device):
+        self.min = lower
+        self.max = upper
+        self.steps = steps
+        t = torch.arange(self.steps, device=device) / self.steps
+        s = 0.008
+        ft = torch.cos((t + s) / (1 + s) * pi * 0.5).square()
+        self._alpha_prod = ft / ft[0]
+
+        self._alphas = torch.empty_like(self._alpha_prod)
+        self._alphas[0] = self._alpha_prod[0]
+        self._alphas[1:] = self._alpha_prod[1:] / self._alpha_prod[:-1]
+        self._betas = 1 - self._alphas
