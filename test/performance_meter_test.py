23beb0564da42f8932fd368b644577064c672429
mibbim <michele.berti.g@gmail.com>
2022-07-22 11:10:42 +0200

    Updated test

--- test/performance_meter_test.py
+++ test/performance_meter_test.py
@@ -3,13 +3,15 @@ from scripts.performance_meter import AverageMeter
 
 def test_avg_meter():
     am = AverageMeter(["mse", "vlb"])
-    print(f"Instatiated with mse and vlb: \n", am.metrics)
     am.update({"mse": 10, "vlb": 3})
-    print(f"Updated with 10, and 3: \n", am.metrics)
+    assert am.metrics == {'mse': {'val': 10, 'count': 1, 'sum': 10, 'avg': 10.0},
+                          'vlb': {'val': 3, 'count': 1, 'sum': 3, 'avg': 3.0}}
     am.update({"mse": 30, "vlb": 5})
-    print(f"Updated with 30 and 5: \n", am.metrics)
+    assert am.metrics == {'mse': {'val': 30, 'count': 2, 'sum': 40, 'avg': 20.0},
+                          'vlb': {'val': 5, 'count': 2, 'sum': 8, 'avg': 4.0}}
     am.reset()
-    print(f"Reset: \n", am.metrics)
+    assert am.metrics == {'mse': {'val': 0, 'count': 0, 'sum': 0, 'avg': 0},
+                          'vlb': {'val': 0, 'count': 0, 'sum': 0, 'avg': 0}}
 
 
 if __name__ == "__main__":
